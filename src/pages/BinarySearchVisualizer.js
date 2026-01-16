813954d4c065b3869357f7c6f3c8d9039738c32e
AnyaH0831 <anyahuang0831@gmail.com>
2026-01-16 02:43:25 -0500

    basic animations

--- src/pages/BinarySearchVisualizer.js
+++ src/pages/BinarySearchVisualizer.js
@@ -4,7 +4,7 @@ function BinarySearchVisualizer(){
 
     //State Management
     const [array, setArray] = useState([7, 14, 21, 28, 35, 42, 49, 56, 63, 70]);
-    const [target, setTarget] = useState(35);
+    const [target, setTarget] = useState(28);
 
     const [currentStep, setCurrentStep] = useState(0);
     const [isPlaying, setIsPlaying] = useState(false);
@@ -83,6 +83,51 @@ function BinarySearchVisualizer(){
         setRight(array.length - 1);
         setMid(Math.floor((0+array.length - 1) / 2));
     }
+
+    useEffect(() => {
+        if (!isPlaying) return;
+        if (left>right){
+            setIsPlaying(false);
+            return;
+        }
+
+        if (array[mid] === target) {
+            setIsPlaying(false);
+            alert(`Found ${target} at index ${mid}!`);
+            return;
+        }
+
+        const timer = setTimeout(() => {
+            if (array[mid] < target){
+                setLeft(mid+1);
+            }else{
+                setRight(mid - 1);
+            }
+
+            let newLeft;
+            if (array[mid] < target){
+                newLeft = mid + 1;
+            }else{
+                newLeft = left;
+            }
+
+            let newRight;
+            if (array[mid] < target){
+                newRight = right;
+            }else{
+                newRight = mid - 1;
+            }
+            setMid(Math.floor((newLeft + newRight) /2));
+            setLeft(newLeft);
+            setRight(newRight);
+            setCurrentStep(currentStep + 1);
+
+        }, 1000) //delay
+        return () => clearTimeout(timer);
+        
+    }, [isPlaying,left,right,mid,currentStep, array, target, speed]); 
+
+    
 
 
     return (
